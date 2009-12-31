59e344131e0282daedf29f7802b187bcf28b25bf
Tom Joseph <ttjoseph@gmail.com>
2009-12-31 12:37:52 -0600

    Remove Flash ads in hope that Chrome will decide not to load the plugin if no other Flash is on the page. More specific Flash selectors in initial-block.js

--- initial-block.js
+++ initial-block.js
@@ -1,11 +1,12 @@
 var elemhideSelectorsString = null; // Cache the elemhide selectors
+var FLASH_SELECTORS = 'embed[type*="application/x-shockwave-flash"],embed[src*=".swf"],object[type*="application/x-shockwave-flash"],object[codetype*="application/x-shockwave-flash"],object[src*=".swf"],object[codebase*="swflash.cab"],object[classid*="D27CDB6E-AE6D-11cf-96B8-444553540000"],object[classid*="d27cdb6e-ae6d-11cf-96b8-444553540000"]';
 
 var styleElm = document.createElement("style");
 styleElm.title = "__adthwart__"; // So we know which one to remove later
 chrome.extension.sendRequest({reqtype: "get-experimental-enabled-state"}, function(response) {
     if(response.enabled && response.experimentalEnabled) {
         elemhideSelectorsString = response.selectors.join(",");
-        styleElm.innerText = "img, iframe { visibility: hidden } object, embed { display: none } " + elemhideSelectorsString + " { visibility: hidden }";
+        styleElm.innerText = "img, iframe { visibility: hidden !important } " + FLASH_SELECTORS + " { display: none !important } " + elemhideSelectorsString + " { visibility: hidden !important }";
     }
     document.documentElement.insertBefore(styleElm, null);
 });
