8f4bf39fd5a4e18e6b8da29b42095c0ae9b6bd08
Tom Joseph <ttjoseph@gmail.com>
2010-01-09 02:59:23 -0500

    Remove individual stylesheet rules instead of using multiple stylesheets which aren't actually registered
    --HG--
    extra : convert_revision : baf87089126bfa8e67f9a2b2355cf9aa35126903

--- initial-block.js
+++ initial-block.js
@@ -1,18 +1,20 @@
 var elemhideSelectorsString = null; // Cache the elemhide selectors
-var FLASH_SELECTORS = 'embed[type*="application/x-shockwave-flash"],embed[src*=".swf"],object[type*="application/x-shockwave-flash"],object[codetype*="application/x-shockwave-flash"],object[src*=".swf"],object[codebase*="swflash.cab"],object[classid*="D27CDB6E-AE6D-11cf-96B8-444553540000"],object[classid*="d27cdb6e-ae6d-11cf-96b8-444553540000"]';
+var FLASH_SELECTORS = '__adthwart__, embed[type*="application/x-shockwave-flash"],embed[src*=".swf"],object[type*="application/x-shockwave-flash"],object[codetype*="application/x-shockwave-flash"],object[src*=".swf"],object[codebase*="swflash.cab"],object[classid*="D27CDB6E-AE6D-11cf-96B8-444553540000"],object[classid*="d27cdb6e-ae6d-11cf-96b8-444553540000"]';
 
 // Use separate style elements for hiding all images and Flash (will be disabled later)
 // and elemhide filters (won't be disabled later)
 var styleElm = document.createElement("style");
 styleElm.title = "__adthwart__"; // So we know which one to remove later
-var elemhideStyleElm = document.createElement("style");
-elemhideStyleElm.title = "__adthwart__elemhide";
+//var elemhideStyleElm = document.createElement("style");
+//elemhideStyleElm.title = "__adthwart__elemhide";
 chrome.extension.sendRequest({reqtype: "get-experimental-enabled-state"}, function(response) {
     if(response.enabled && response.experimentalEnabled) {
         elemhideSelectorsString = response.selectors.join(",");
-        styleElm.innerText = "img { visibility: hidden !important } iframe { display: none !important } " + FLASH_SELECTORS + " { display: none !important } ";
-        elemhideStyleElm.innerText = elemhideSelectorsString + " { display: none !important }";
+        styleElm.innerText = "__adthwart__, img { visibility: hidden !important } __adthwart__, iframe { display: none !important } " + FLASH_SELECTORS + " { display: none !important } ";
+        // elemhideStyleElm.innerText = elemhideSelectorsString + " { display: none !important }";
+        styleElm.innerText = styleElm.innerText + elemhideSelectorsString + " { display: none !important }";
     }
+    // Wow. Apparently we can't inject more than one style element; only the first one will stick
+    //document.documentElement.insertBefore(elemhideStyleElm, null);
     document.documentElement.insertBefore(styleElm, null);
-    document.documentElement.insertBefore(elemhideStyleElm, null);
 });
