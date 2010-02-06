4a9f7306da877ee240fd464b6b0330953452ca27
Tom Joseph <ttjoseph@gmail.com>
2010-02-06 17:05:34 -0500

    Instead of using one gigantic selector string, split elemhide selectors into much smaller groups and have more CSS rules. The giant selector string exposed pathological behavior from WebKit's selector engine in certain cases.
    --HG--
    extra : convert_revision : 87314d18ce2a1b9f8e94c941c6be71d583ee36f1

--- initial-block.js
+++ initial-block.js
@@ -1,22 +1,37 @@
 // This file (c) T. Joseph <ttjoseph@gmail.com>
 // Everyone can use, modify and distribute this file without restriction.
 
-var elemhideSelectorsString = null; // Cache the elemhide selectors
+var elemhideSelectorStrings = []; // Cache the elemhide selector strings
+var SELECTOR_GROUP_SIZE = 20;
 var FLASH_SELECTORS = 'embed[type*="application/x-shockwave-flash"],embed[src*=".swf"],object[type*="application/x-shockwave-flash"],object[codetype*="application/x-shockwave-flash"],object[src*=".swf"],object[codebase*="swflash.cab"],object[classid*="D27CDB6E-AE6D-11cf-96B8-444553540000"],object[classid*="d27cdb6e-ae6d-11cf-96b8-444553540000"]';
 
-// Use separate style elements for hiding all images and Flash (will be disabled later)
-// and elemhide filters (won't be disabled later)
+// WebKit apparently chokes when the selector list in a CSS rule is huge.
+// So we split the elemhide selectors into groups.
+function makeSelectorStrings(selectors) {
+    var ptr = 0;
+    for(i = 0; i < selectors.length; i += SELECTOR_GROUP_SIZE) {
+        elemhideSelectorStrings[ptr++] = selectors.slice(i, i + SELECTOR_GROUP_SIZE).join(",");
+    }
+    elemhideSelectorsString = selectors.join(",");
+}
+
+// Makes a string containing CSS rules for elemhide filters
+function getElemhideCSSString() {
+    var s = "";
+    for(i in elemhideSelectorStrings) {
+        s += elemhideSelectorStrings[i] + " { display: none !important } ";
+    }
+    return s;
+}
+
+// Use a style element for elemhide selectors and to hide page elements that might be ads.
+// We'll remove the latter CSS rules later.
 var styleElm = document.createElement("style");
 styleElm.title = "__adthwart__"; // So we know which one to remove later
-//styleElm.innerText = "img { visibility: hidden !important } iframe { display: none !important } " + FLASH_SELECTORS + " { display: none !important } ";
-styleElm.innerText = "";
 
-var elemhideStyleElm = document.createElement("style");
-elemhideStyleElm.title = "__adthwart__elemhide";
 chrome.extension.sendRequest({reqtype: "get-initialhide-options"}, function(response) {
+    makeSelectorStrings(response.selectors);
     if(response.enabled) {
-        elemhideSelectorsString = response.selectors.join(",");
-        elemhideStyleElm.innerText = elemhideSelectorsString + " { display: none !important }";
         if(response.initialHideImg)
             styleElm.innerText += "img { visibility: hidden !important } ";
         if(response.initialHideFlash && !document.domain.match(/youtube.com$/i)) {
@@ -28,7 +43,7 @@ chrome.extension.sendRequest({reqtype: "get-initialhide-options"}, function(resp
         }
         if(response.initialHideIframe)
             styleElm.innerText += "iframe { visibility: hidden !important } ";
-        styleElm.innerText += elemhideSelectorsString + " { display: none !important }";
+        styleElm.innerText += getElemhideCSSString();
         document.documentElement.insertBefore(styleElm, null);
     }
 });
