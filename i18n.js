2118c9c93caba0ac8570f871edb4f7c05b5c9eb7
Tom Joseph <ttjoseph@gmail.com>
2010-05-16 13:01:48 -0400

    Add i18n.js
    --HG--
    extra : convert_revision : e74b73fdec3d85fa49e6f2b42da7994e999e118f

---
+++ i18n.js
@@ -0,0 +1,11 @@
+// Loads and inserts i18n strings. Assumes jQuery is loaded.
+function loadI18nStrings_jquery() {
+    $("[id^='i18n_']").each(function(i) { $(this).html(chrome.i18n.getMessage(this.id.substring(5))); });
+}
+
+function loadI18nStrings() {
+    var nodes = document.querySelectorAll("[class^='i18n_']");
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5));
+    }
+}
