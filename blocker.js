8a5e6b8d67bb422ca9b48f483b33c43043f59194
Tom Joseph <ttjoseph@gmail.com>
2010-05-27 14:01:31 -0400

    Further clickhide dialog appearance tweaks

--- blocker.js
+++ blocker.js
@@ -143,7 +143,7 @@ function clickHide_showDialog(left, top, filters) {
         
     clickHideFiltersDialog = document.createElement('div');
     clickHideFiltersDialog.setAttribute('style', '-webkit-user-select:none ; font-family: Helvetica,Arial,sans-serif !important; font-size: 10pt ; position: fixed; left:' + left + 'px; top:' + top + 'px ; max-width: 400px ; -webkit-box-shadow: 5px 5px 20px rgba(0,0,0,0.5); background: #ffffff; z-index: 99999; padding: 10px; border-radius: 5px');
-    clickHideFiltersDialog.innerHTML = '<table><tr><td style="vertical-align: top; padding-right: 5px"><img src="' + chrome.extension.getURL('icons/face-devilish-32.png') + '"/></td><td style="vertical-align: top">' + chrome.i18n.getMessage('add_filters_msg') + '<br/><div style="margin-top:5px; border:1px solid #c0c0c0; padding:3px; overflow:auto; max-width: 350px; font-size:8pt !important; line-height: 10pt !important; font-color: #909090 !important; background: #ffffff !important">' + filtersString + '</div></td></tr></table>';
+    clickHideFiltersDialog.innerHTML = '<table><tr><td style="padding-right: 5px"><img src="' + chrome.extension.getURL('icons/face-devilish-32.png') + '"/></td><td>' + chrome.i18n.getMessage('add_filters_msg') + '</td></tr></table><div style="border:1px solid #c0c0c0; padding:3px; min-width: 200px; max-width: 350px; font-size:8pt !important; line-height: 10pt !important; font-color: #909090 !important; background: #ffffff !important">' + filtersString + '</div>';
 
     buttonsDiv = document.createElement('div');
     buttonsDiv.setAttribute('style', 'text-align: right');
