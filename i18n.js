9c1c145858e5e2d4ac938851c3481853cf95589f
Tom Joseph <ttjoseph@gmail.com>
2010-05-29 08:04:45 -0400

    Use chrome.i18n.getMessage() sprintf-like functionality instead of custom sprintf()

--- i18n.js
+++ i18n.js
@@ -1,17 +1,13 @@
-// Loads and inserts i18n strings. Assumes jQuery is loaded.
-function loadI18nStrings_jquery() {
-    $("[id^='i18n_']").each(function(i) { $(this).html(chrome.i18n.getMessage(this.id.substring(5))); });
-}
-
+// Loads and inserts i18n strings into matching elements. Any inner HTML already in the
+// element is parsed as JSON and used as parameters to substitute into placeholders in the
+// i18n message.
 function loadI18nStrings() {
     var nodes = document.querySelectorAll("[class^='i18n_']");
     for(var i = 0; i < nodes.length; i++) {
 		var arguments = JSON.parse("[" + nodes[i].innerHTML + "]");
-		nodes[i].innerHTML = i18n_sprintf(nodes[i].className.substring(5), arguments);			
+		if(arguments.length > 0)
+		    nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5), arguments);
+		else
+		    nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5));
     }
-}
-
-function i18n_sprintf(msg, arr)
-{ 
-	return sprintf(chrome.i18n.getMessage(msg), arr);
 }
