94bf7ef3cc686fc641688d10afdce999b63fa496
Tom Joseph <ttjoseph@gmail.com>
2010-07-25 11:26:41 -0400

    [i18n:fr] Translation time/date tweaks (including: 13h00 instead of 1:00 p.m.)
    --HG--
    extra : convert_revision : cb29209d045b2e6d06176f1c4ababbae72792d70

--- i18n.js
+++ i18n.js
@@ -11,3 +11,34 @@ function loadI18nStrings() {
 		    nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5));
     }
 }
+
+function i18n_time(h, m) {
+    locale = chrome.i18n.getMessage("@@ui_locale");
+	if(m < 10) m = "0" + m;
+    if(locale == "fr") {
+        return h + "h" + m;
+    } else {
+        var ampm = "a.m.";
+    	if(h > 12) {
+    		h -= 12;
+    		ampm = "p.m.";
+    	}
+    	if(h == 0) h = 12;
+    	return(h + ":" + m + " " + ampm);
+    }
+}
+
+// Provides a more readable string of the current date and time
+function i18n_timeDateStrings(when) {
+	var monthNames = JSON.parse(chrome.i18n.getMessage("month_names"));
+	var d = new Date(when);
+	var timeString = i18n_time(d.getHours(), d.getMinutes());
+	var now = new Date();
+	if(d.getDate() == now.getDate() && d.getMonth() == now.getMonth() && d.getFullYear() == now.getFullYear())
+		dateString = chrome.i18n.getMessage("today");
+	else
+		dateString = chrome.i18n.getMessage("date_format", [d.getDate(), monthNames[d.getMonth()], d.getFullYear()]);
+	
+	return [timeString, dateString];
+}
+
