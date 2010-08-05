67a5a0c54b02b9dff21e3240221fc0735d1fe5e9
Tom Joseph <ttjoseph@gmail.com>
2010-08-04 22:34:14 -0400

    Whitespace and cosmetic change
    --HG--
    extra : convert_revision : acae77168e131e66dfc6007266525b13fff128f5

--- i18n.js
+++ i18n.js
@@ -4,11 +4,11 @@
 function loadI18nStrings() {
     var nodes = document.querySelectorAll("[class^='i18n_']");
     for(var i = 0; i < nodes.length; i++) {
-		var arguments = JSON.parse("[" + nodes[i].innerHTML + "]");
-		if(arguments.length > 0)
-		    nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5), arguments);
-		else
-		    nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5));
+        var arguments = JSON.parse("[" + nodes[i].innerHTML + "]");
+        if(arguments.length > 0)
+            nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5), arguments);
+        else
+            nodes[i].innerHTML = chrome.i18n.getMessage(nodes[i].className.substring(5));
     }
 }
 
@@ -19,26 +19,26 @@ function i18n_time(h, m) {
         return h + "h" + m;
     } else {
         var ampm = "a.m.";
-    	if(h >= 12) {
+        if(h >= 12) {
             h -= 12;
             ampm = "p.m.";
-    	}
-    	if(h == 0) h = 12;
-    	return(h + ":" + m + " " + ampm);
+        }
+        if(h == 0) h = 12;
+        return(h + ":" + m + " " + ampm);
     }
 }
 
 // Provides a more readable string of the current date and time
 function i18n_timeDateStrings(when) {
-	var monthNames = JSON.parse(chrome.i18n.getMessage("month_names"));
-	var d = new Date(when);
-	var timeString = i18n_time(d.getHours(), d.getMinutes());
-	var now = new Date();
-	if(d.getDate() == now.getDate() && d.getMonth() == now.getMonth() && d.getFullYear() == now.getFullYear())
+    var monthNames = JSON.parse(chrome.i18n.getMessage("month_names"));
+    var d = new Date(when);
+    var timeString = i18n_time(d.getHours(), d.getMinutes());
+    var now = new Date();
+    if(d.getDate() == now.getDate() && d.getMonth() == now.getMonth() && d.getFullYear() == now.getFullYear())
             dateString = chrome.i18n.getMessage("today");
-	else
+    else
             dateString = chrome.i18n.getMessage("date_format", [d.getDate(), monthNames[d.getMonth()], d.getFullYear()]);
-	
-	return [timeString, dateString];
+    
+    return [timeString, dateString];
 }
 
