8814095cf23d5326a32532c7e2894700d106af39
Tom Joseph <ttjoseph@gmail.com>
2010-09-23 16:28:55 -0400

    When downloading from AdThwart server, compare MD5 hashes of local copy of filter list with remote to avoid unnecessarily downloading entire filter list. This is to save bandwidth for the server, allowing for more frequent updates.
    --HG--
    extra : convert_revision : 6d1cac9d0ac439c383126abff8b9bd25b3c067cd

--- filterupdate.js
+++ filterupdate.js
@@ -89,6 +89,15 @@ function loadUserFilterURLs() {
         filterFiles[key] = urls[key];
 }
 
+// Filter update protocol:
+// 
+// When fetching a filter list, we send an HTTP header X-AdThwart-My-MD5 with the MD5 digest of
+// our local copy of the filter list. The server uses that to decide whether we already have the
+// latest version. If so, it sends a string starting with "AdThwart-OK:". We can just keep
+// the data we already have in this case.
+// This saves bandwidth for the AdThwart server without interfering with non-AdThwart filter 
+// list servers.
+
 // TODO: In case of error fetching a filter list, check to see whether
 // we already have a copy cached, and leave it there.
 // At present the cached copy can be deleted.
@@ -103,16 +112,37 @@ function FilterListFetcher(nameOrUrl, callback) {
     this.xhr.onreadystatechange = function() {
         if(this.readyState != 4) return;
         if(this.status == 200) {
-            // Check if it's actually a filter set
+            // If server thinks we are up to date based on the MD5 has we reported, just nudge
+            // the last updated time and call it a day
+            if(this.responseText.match(/^AdThwart-OK\:/)) {
+                var now = (new Date()).getTime();
+                var currentFilters = JSON.parse(localStorage[fetcher.url]);
+                localStorage[fetcher.url] = JSON.stringify({lastDownloaded: now, lastUpdated: currentFilters.lastUpdated, expires: currentFilters.expires, text: currentFilters.text});
+                // console.log("Local copy up to date, not redownloading", fetcher.url);
+                fetcher.callback(fetcher);
+                return;
+            }
+            
+            // Check if it's actually a filter set and if so, save it along with its expiry information
             if(this.responseText.match(/\[Adblock/)) {
                 var lastUpdated = this.responseText.match(/Last modified:\s+(.+)/i);
                 var now = (new Date()).getTime();
                 lastUpdated = lastUpdated ? Date.parse(lastUpdated[1]) : now;
-                var expires = this.responseText.match(/Expires:\s+(\d+) day/i);
-                expires = expires ? parseInt(expires[1]) * 86400 * 1000 : DEFAULT_EXPIRE_TIME; // Milliseconds in n days
+                var expires = this.responseText.match(/Expires:\s+(\d+) (.+)/i);
+                var unit = "day";
+                var unitLength = 86400; // Default to units of days
+                if(!expires) expires = ["", "3", "days"]; // Default to 3 days if Expires field is unparseable
+                if(expires.length > 2) unit = expires[2]; // Is a unit specified? If so, grab it
+                if(unit.match(/hour/)) unitLength = 3600; // in seconds
+                expires = expires ? parseInt(expires[1]) * unitLength * 1000 : DEFAULT_EXPIRE_TIME; // in milliseconds
                 // Clamp the expire time to a predefined range to defend against bad input
-                expires = expires > MAX_EXPIRE_TIME ? MAX_EXPIRE_TIME : expires;
-                expires = expires < MIN_EXPIRE_TIME ? MIN_EXPIRE_TIME : expires; 
+                // unless it's the AdThwart server, which we trust. This is a really weak
+                // authentication mechanism but the worst a lying server could do is
+                // cause a DDoS on itself
+                if(this.getResponseHeader("X-AdThwart") == "") {
+                    expires = expires > MAX_EXPIRE_TIME ? MAX_EXPIRE_TIME : expires;
+                    expires = expires < MIN_EXPIRE_TIME ? MIN_EXPIRE_TIME : expires;
+                }
                 // If the list we just downloaded is expired, mark its lastUpdated time as now or we will
                 // think the list is too old on every update check and keep trying to download it, which would pound
                 // that server, which wouldn't be very nice. Also, if the list claims it was updated in the
@@ -137,10 +167,21 @@ function FilterListFetcher(nameOrUrl, callback) {
         // TODO: Doesn't actually do anything in case of other errors
     }
     try {
+        // If we already have a copy of this filter list, calculate its MD5 hash.
+        // We'll send this in an HTTP header. This way, the server doesn't have to
+        // send the whole filter list if the client already has it.
+        var currentFilters = null, md5hash = null;
+        if(typeof localStorage[this.url] == "string") currentFilters = JSON.parse(localStorage[this.url]);
+        if(currentFilters && currentFilters.text) md5hash = MD5.hash(currentFilters.text);
+        // Have to call open() before setting the header for some reason
         this.xhr.open("GET", this.url, true);
+        if(md5hash) {
+            // console.log(md5hash, currentFilters.text.length);
+            this.xhr.setRequestHeader("X-AdThwart-My-MD5", md5hash);
+        }
         this.xhr.send(null);
     } catch(e) {
-        fetcher.error = "Error. Hm.";
+        fetcher.error = "Useless error message: " + e;
         fetcher.callback(fetcher);        
     }
 }
