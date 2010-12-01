8a75092915fbfb18d597db1083422b27aa902659
Wladimir Palant <trev@adblockplus.org>
2010-12-01 17:16:58 +0100

    Put files copyrighted by Tom Joseph under MPL

--- filterupdate.js
+++ filterupdate.js
@@ -1,5 +1,27 @@
-// This file (c) T. Joseph <ttjoseph@gmail.com>
-// Everyone can use, modify and distribute this file without restriction.
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Adblock Plus for Chrome.
+ *
+ * The Initial Developer of the Original Code is
+ * T. Joseph <ttjoseph@gmail.com>.
+ * Portions created by the Initial Developer are Copyright (C) 2009-2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Wladimir Palant
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 // List of suggested filter lists
 var filterFiles = {
