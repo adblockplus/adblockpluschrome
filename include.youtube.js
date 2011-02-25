5b6051fa5db202130abf78e95f5f63e1b254b144
Wladimir Palant <trev@adblockplus.org>
2011-02-25 14:24:21 +0100

    Rewrite parameters of the YouTube video player even if the response from background process comes late (patch by Tom Joseph with minor changes)

--- include.youtube.js
+++ include.youtube.js
@@ -24,35 +24,64 @@
  * ***** END LICENSE BLOCK ***** */
 
 // YouTube special case option
+var shouldRemoveYouTubeAds = undefined;
+var savedPlayer, savedPlayerVars;
 
+function stripYouTubePlayerAds(player, newVars)
+{
+  player.setAttribute("flashvars", newVars.join("&"));
+
+  // Remove the node and insert it back, the variables won't be reloaded otherwise
+  var parent = player.parentNode;
+  var insertBefore = player.nextSibling;
+  parent.removeChild(player);
+  parent.insertBefore(player, insertBefore);
+}
+
+function onYouTubeBeforeLoad(e)
+{
+  var eltDomain = extractDomainFromURL(e.url);
+  var player = e.target;
+  if (player && /\bytimg\.com$/.test(eltDomain) &&
+      /^(embed|object)$/.test(player.localName) &&
+      player.hasAttribute("flashvars"))
+  {
+    // Remove a bunch of known parameters from flashvars attribute of the player
+    var flashVars = player.getAttribute("flashvars").split("&");
+    var newVars = [];
+    for (var i = 0; i < flashVars.length; i++)
+      if (!/^(ad\d*_|instream|infringe|invideo|interstitial|mpu|prerolls|tpas_ad_type_id|trueview|watermark)/.test(flashVars[i]))
+        newVars.push(flashVars[i]);
+    if (newVars.length != flashVars.length)
+    {
+      if (typeof shouldRemoveYouTubeAds == "undefined")
+      {
+        // If enabled-check hasn't returned yet, save the event and new flashvars so that ad removal
+        // can occur when it does return. We ought only to need to save one since there will only be
+        // one video player loaded at a time.
+        savedPlayer = player;
+        savedPlayerVars = newVars;
+      }
+      else if (shouldRemoveYouTubeAds)
+        stripYouTubePlayerAds(player, newVars);
+    }
+  }
+}
+
+// Ask the backend whether we are enabled. This may take a while, so we attach the beforeload listener
+// no matter what. If this callback hasn't been invoked by the time we need to act on the YouTube
+// player object, the beforeload handler will just save the target object and new flashvars so that
+// this callback can modify the flashvars and reload the object.
 chrome.extension.sendRequest({reqtype: "get-domain-enabled-state"}, function(response)
 {
-  if (response.enabled && response.specialCaseYouTube)
+  shouldRemoveYouTubeAds = (response.enabled && response.specialCaseYouTube);
+  if (shouldRemoveYouTubeAds)
   {
-    document.addEventListener("beforeload", function(e)
-    {
-      var eltDomain = extractDomainFromURL(e.url);
-      if (e.target && /\bytimg\.com$/.test(eltDomain) &&
-          /^(embed|object)$/.test(e.target.localName) &&
-          e.target.hasAttribute("flashvars"))
-      {
-        // Remove a bunch of known parameters from flashvars attribute of the player
-        var flashVars = e.target.getAttribute("flashvars").split("&");
-        var newVars = [];
-        for (var i = 0; i < flashVars.length; i++)
-          if (!/^(ad\d*_|instream|infringe|invideo|interstitial|mpu|prerolls|tpas_ad_type_id|trueview|watermark)/.test(flashVars[i]))
-            newVars.push(flashVars[i]);
-        if (newVars.length != flashVars.length)
-        {
-          e.target.setAttribute("flashvars", newVars.join("&"));
-
-          // Remove the node and insert it back, the variables won't be reloaded otherwise
-          var parent = e.target.parentNode;
-          var insertBefore = e.target.nextSibling;
-          parent.removeChild(e.target);
-          parent.insertBefore(e.target, insertBefore);
-        }
-      }
-    }, true);
+    if (savedPlayer)
+      stripYouTubePlayerAds(savedPlayer, savedPlayerVars);
   }
+  else
+    document.removeEventListener("beforeload", onYouTubeBeforeLoad, true);
 });
+
+document.addEventListener("beforeload", onYouTubeBeforeLoad, true);
