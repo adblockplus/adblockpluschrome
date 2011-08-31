ca904906947803dd84445cf442b80752e0b6b968
Wladimir Palant <trev@adblockplus.org>
2011-08-31 17:06:40 +0200

    Added list of test URLs for WebKit bug 45586 to the repository now that I have it anyway

---
+++ webkit_bug_45586_urls.txt.sh
@@ -0,0 +1,27 @@
+Test URLs for WebKit bug 45586:
+
+http://www.redfin.com/NY/Oakland-Gardens/6139-224th-St-11364/Uppr/home/39601225
+http://www.shop.ww.kz/catalog/pc-parts/mb/asus_p8z68_v_intel_z68/132648.html
+http://www.wien.gv.at/verkehr/strassen/bauen/grossprojekte/landstrasserguertel.html
+http://www.lojagloboesporte.com/casual/tenis-adidas-campus-ii/prod132-3142-114.html
+http://www.netshoes.com.br/produto/020-0023-120-39
+http://www.victorinox.com/category/2/200/2003/2101?f=category&v=2/200/2003/2101&m=add&
+http://www.clove.co.uk/blackberry-bold-9900-dakota
+http://www.mtonline.ru/catalog/vidtec/kinoteatr/panasonic_sc_pt465ee_s/
+http://www.avikomp.ru/torgovaya-marka-akkurat/produktsiya-iz-mikrofibry.html
+http://mp3dostavka.ru/product_2614.html
+http://notebookhp.ru/product_1483.html
+http://bestrepack.net/forum/viewtopic.php?t=6919
+http://www.e-shop.gr/show_per.phtml?id=PER.650913
+http://www.satchef.de/Satelliten-Receiver-HDTV-Common-Interface-PVR-Ready
+http://www.ebuyer.com/200488-dell-1130-mono-laser-printer-210-31795
+http://prod.projetweb.asb.nbs-test.com/polo-manches-longue-uni.html?color=6
+http://www.brueckenkopf-online.com/?p=42127
+
+Libraries involved:
+
+Highslide
+Magic Zoom
+Magic Zoom Plus
+jQZoom
+ColorBox
