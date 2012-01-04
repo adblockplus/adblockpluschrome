408603649f2d34d849567a074ff123855cc5e96b
Wladimir Palant <trev@adblockplus.org>
2012-01-04 09:41:17 +0100

    Relicensed from MPL 1.1 to MPL 2.0

--- i18n.js
+++ i18n.js
@@ -1,27 +1,8 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Adblock Plus for Chrome.
- *
- * The Initial Developer of the Original Code is
- * T. Joseph <tom@adblockplus.org>.
- * Portions created by the Initial Developer are Copyright (C) 2009-2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *  Wladimir Palant
- *
- * ***** END LICENSE BLOCK ***** */
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
 
 // Loads and inserts i18n strings into matching elements. Any inner HTML already in the
 // element is parsed as JSON and used as parameters to substitute into placeholders in the
