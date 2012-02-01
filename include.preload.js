0c5c4aac9644e876f0371e415ac0c2a61a360f07
Wladimir Palant <trev@adblockplus.org>
2012-02-01 13:53:53 +0100

    Backed out changeset a4828ee605da (Topic 9464 - Changed the work-around for Chrome bug 49001, use a stylesheet URL for element hiding that is considered first-party) - causes a warning on HTTPS pages

--- include.preload.js
+++ include.preload.js
@@ -35,7 +35,7 @@ function setElemhideCSSRules(selectors)
   elemhideElt = document.createElement("link");
   elemhideElt.setAttribute("rel", "stylesheet");
   elemhideElt.setAttribute("type", "text/css");
-  elemhideElt.setAttribute("href", "javascript://");
+  elemhideElt.setAttribute("href", "data:text/css,/*http://*/");
   document.documentElement.appendChild(elemhideElt);
 
   var elt = elemhideElt;  // Use a local variable to avoid racing conditions
