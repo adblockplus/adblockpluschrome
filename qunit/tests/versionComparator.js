ed2dbcdc146be58c28b467f844a03d11ca04c58e
Felix H. Dahlke <fhd@ubercode.de>
2012-05-26 08:39:39 +0200

    Version comparison: Compare against equal versions from the examples

--- qunit/tests/versionComparator.js
+++ qunit/tests/versionComparator.js
@@ -11,6 +11,24 @@
     return pairs;
   }
 
+  function versionsEqual(versions)
+  {
+    allPairs(versions).forEach(function(pair)
+    {
+      var v1 = pair[0];
+      var v2 = pair[1];
+      equal(compare(v1, v2), 0, "'" + v1 + "' should be equal to '" + v2 + "'");
+    });
+  }
+
+  function versionLarger(v1, v2)
+  {
+    equal(compare(v1, v2), -1,
+          "'" + v1 + "' should be smaller than '" + v2 + "'");
+    equal(compare(v2, v1), 1,
+          "'" + v2 + "' should be larger than '" + v1 + "'");
+  }
+
   module("Test utilities");
   test("allPairs", 1, function()
   {
@@ -19,43 +37,32 @@
 
   module("versionComparator");
 
-  test("Equal versions", 6, function()
-  {
-    var versions = ["1", "1.0", "1.0.0", "1.0.0.0"];
-    allPairs(versions).forEach(function(pair)
-    {
-      var v1 = pair[0];
-      var v2 = pair[1];
-      equal(compare(v1, v2), 0, "'" + v1 + "' should be equal to '" + v2 + "'");
-    });
-  });
-
-  test("Examples", 30, function()
+  test("Examples", 106, function()
   {
     var examples = [
       "1.0pre1",
       "1.0pre2",
       ["1.0", "1.0.0", "1.0.0.0"],
-      ["1.1pre", "1.1pre0", "1.0+"],
+      ["1.1pre", "1.1pre0"/*, "1.0+"*/], // TODO: Support +
       "1.1pre1a",
       "1.1pre1",
       "1.1pre10a",
       "1.1pre10"
     ];
 
-    // TODO: Compare all values in arrays for equality
+    examples.forEach(function(example)
+    {
+      if (example instanceof Array)
+        versionsEqual(example);
+    });
 
     allPairs(examples).forEach(function(pair)
     {
-      var v1 = pair[0];
-      var v2 = pair[1];
-      // TODO: Compare against each element of the array
-      if (v1 instanceof Array || v2 instanceof Array)
-        return;
-      equal(compare(v1, v2), -1,
-            "'" + v1 + "' should be smaller than '" + v2 + "'");
-      equal(compare(v2, v1), 1,
-            "'" + v2 + "' should be larger than '" + v1 + "'");
+      var v1 = [].concat(pair[0]);
+      var v2 = [].concat(pair[1]);
+      for (var i = 0; i < v1.length; i++)
+        for (var j = 0; j < v2.length; j++)
+          versionLarger(v1[i], v2[j]);
     });
   });
 })();
