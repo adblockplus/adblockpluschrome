36af2d6101d6b4bd210131a3836db803ade67f46
Wladimir Palant <trev@adblockplus.org>
2012-05-30 14:43:33 +0200

    Don't add unit tests to the build

--- build.py
+++ build.py
@@ -80,7 +80,7 @@ def addToZip(zip, filters, dir, baseName):
   for file in os.listdir(dir):
     filelc = file.lower()
     if (file.startswith('.') or
-        file == 'buildtools' or
+        file == 'buildtools' or file == 'qunit' or
         filelc.endswith('.py') or filelc.endswith('.pyc') or
         filelc.endswith('.crx') or filelc.endswith('.zip') or
         filelc.endswith('.sh') or filelc.endswith('.bat') or
