b4f64df0386490c1896f46fc02cd8a1c34327944
Wladimir Palant <trev@adblockplus.org>
2012-10-26 17:04:22 +0200

    Updated license to GPLv3

--- firstRun.js
+++ firstRun.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Adblock Plus extension,
+ * Copyright (C) 2006-2012 Eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var backgroundPage = chrome.extension.getBackgroundPage();
 var require = backgroundPage.require;
 var Prefs = require("prefs").Prefs;
