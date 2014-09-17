3b7e69ea221bbc81923608ddc42f9e32c6685c7d
Thomas Greiner <thomas@adblockplus.org>
2014-09-17 17:15:40 +0200

    Reverted backout of 57f5dbea9d24

--- include.preload.js
+++ include.preload.js
@@ -58,11 +58,11 @@ function checkCollapse(element)
   }
 }
 
-function checkExceptionKey()
+function checkSitekey()
 {
   var attr = document.documentElement.getAttribute("data-adblockkey");
   if (attr)
-    ext.backgroundPage.sendMessage({type: "add-key-exception", token: attr});
+    ext.backgroundPage.sendMessage({type: "add-sitekey", token: attr});
 }
 
 function hasInlineURL(element, attribute)
@@ -203,6 +203,6 @@ function init(document)
 
 if (document instanceof HTMLDocument)
 {
-  checkExceptionKey();
+  checkSitekey();
   init(document);
 }
