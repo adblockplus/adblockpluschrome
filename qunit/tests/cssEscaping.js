daa6e2acd3d73019779aa620a93292e485b5aa53
Sebastian Noack <sebastian@adblockplug.org>
2014-11-20 14:52:27 +0100

    Issue 1587 - Escape curly brackets for elemhide filters
    --HG--
    extra : rebase_source : b901b6dd6f678e8d3813d6ae837eebe136fbfbba

--- qunit/tests/cssEscaping.js
+++ qunit/tests/cssEscaping.js
@@ -1,114 +1,130 @@
-module(
-  "CSS escaping",
-  {
-    setup: function()
-    {
-      var frame = document.createElement("iframe");
-      frame.srcdoc = '<script src="../include.postload.js"></script>';
-
-      stop();
-      frame.addEventListener("load", function()
-      {
-        start();
-
-        this.escapeCSS = frame.contentWindow.escapeCSS;
-        this.quote = frame.contentWindow.quote;
-
-        document.body.removeChild(frame);
-      }.bind(this));
-
-      document.body.appendChild(frame);
-    }
-  }
-);
-
-test("CSS escaping", function()
+(function()
 {
-  var escapeCSS = this.escapeCSS;
-  var quote = this.quote;
+  var filterClasses = require("filterClasses");
+  var Filter = filterClasses.Filter;
+  var ElemHideFilter = filterClasses.ElemHideFilter;
 
-  function testSelector(opts)
-  {
-    var mustMatch = opts.mustMatch !== false;
-    var doc = document.implementation.createHTMLDocument();
-
-    var style = doc.createElement("style");
-    doc.documentElement.appendChild(style);
-    style.sheet.insertRule(opts.selector + " {}", 0);
-
-    var element;
-    try
+  module(
+    "CSS escaping",
     {
-      element = doc.createElement(opts.tagName || "div");
+      setup: function()
+      {
+        var frame = document.createElement("iframe");
+        frame.srcdoc = '<script src="../include.postload.js"></script>';
+
+        stop();
+        frame.addEventListener("load", function()
+        {
+          start();
+
+          this.escapeCSS = frame.contentWindow.escapeCSS;
+          this.quote = frame.contentWindow.quote;
+
+          document.body.removeChild(frame);
+        }.bind(this));
+
+        document.body.appendChild(frame);
+      }
     }
-    catch (e)
-    {
-      // Some characters we are going to test can not occur in tag names,
-      // but we still have to make sure that no exception is thrown when
-      // calling .querySelector() and .insertRule()
-      element = null;
-      mustMatch = false;
-    }
+  );
 
-    if (element)
-    {
-      for (var attr in opts.attributes)
-        element.setAttribute(attr, opts.attributes[attr]);
-
-      doc.documentElement.appendChild(element);
-    }
-
-    var foundElement = doc.querySelector(opts.selector);
-    if (mustMatch)
-      equal(foundElement, element, opts.selector);
-    else
-      ok(true, opts.selector);
-  }
-
-  function testEscape(s)
+  test("CSS escaping", function()
   {
-    testSelector({
-      selector: escapeCSS(s),
-      tagName: s
-    });
+    var escapeCSS = this.escapeCSS;
+    var quote = this.quote;
 
-    testSelector({
-      selector: "#" + escapeCSS(s),
-      attributes: {id: s}
-    });
+    function testSelector(opts)
+    {
+      var mustMatch = opts.mustMatch !== false;
+      var doc = document.implementation.createHTMLDocument();
 
-    testSelector({
-      selector: "." + escapeCSS(s),
-      attributes: {class: s},
+      var style = doc.createElement("style");
+      doc.documentElement.appendChild(style);
+      style.sheet.insertRule(opts.selector + " {}", 0);
 
-      // Whitespace characters split the class name, hence the selector
-      // won't match. But we still have to make sure that no exception
-      // is thrown when calling .querySelector() and .insertRule()
-      mustMatch: !/\s/.test(s)
-    });
+      var element;
+      try
+      {
+        element = doc.createElement(opts.tagName || "div");
+      }
+      catch (e)
+      {
+        // Some characters we are going to test can not occur in tag names,
+        // but we still have to make sure that no exception is thrown when
+        // calling .querySelector() and .insertRule()
+        element = null;
+        mustMatch = false;
+      }
 
-    testSelector({
-      selector: "[foo=" + quote(s) + "]",
-      attributes: {foo: s}
-    });
-  }
+      if (element)
+      {
+        for (var attr in opts.attributes)
+          element.setAttribute(attr, opts.attributes[attr]);
 
-  for (var i = 0; i < 0x80; i++)
-  {
-    var chr = String.fromCharCode(i);
+        doc.documentElement.appendChild(element);
+      }
 
-    // Make sure that all ASCII characters are correctly escaped.
-    testEscape(chr);
+      var foundElement = doc.querySelector(opts.selector);
+      var filter = Filter.fromText("##" + opts.selector);
 
-    // Some characters are only escaped when in the first positon,
-    // so we still have to make sure that everything is correctly escaped
-    // in subsequent positions.
-    testEscape("x" + chr);
-   
-    // Leading dashes must be escaped, when followed by certain characters.
-    testEscape("-" + chr);
-  }
+      if (!(filter instanceof ElemHideFilter))
+      {
+        ok(false, opts.selector + " (not allowed in elemhide filters)");
+      }
+      else
+      {
+        if (mustMatch)
+          equal(foundElement, element, opts.selector);
+        else
+          ok(true, opts.selector);
+      }
+    }
 
-  // Test some non-ASCII characters. However, those shouldn't require escaping.
-  testEscape("\uD83D\uDE3B\u2665\u00E4");
-});
+    function testEscape(s)
+    {
+      testSelector({
+        selector: escapeCSS(s),
+        tagName: s
+      });
+
+      testSelector({
+        selector: "#" + escapeCSS(s),
+        attributes: {id: s}
+      });
+
+      testSelector({
+        selector: "." + escapeCSS(s),
+        attributes: {class: s},
+
+        // Whitespace characters split the class name, hence the selector
+        // won't match. But we still have to make sure that no exception
+        // is thrown when calling .querySelector() and .insertRule()
+        mustMatch: !/\s/.test(s)
+      });
+
+      testSelector({
+        selector: "[foo=" + quote(s) + "]",
+        attributes: {foo: s}
+      });
+    }
+
+    for (var i = 0; i < 0x80; i++)
+    {
+      var chr = String.fromCharCode(i);
+
+      // Make sure that all ASCII characters are correctly escaped.
+      testEscape(chr);
+
+      // Some characters are only escaped when in the first positon,
+      // so we still have to make sure that everything is correctly escaped
+      // in subsequent positions.
+      testEscape("x" + chr);
+
+      // Leading dashes must be escaped, when followed by certain characters.
+      testEscape("-" + chr);
+    }
+
+    // Test some non-ASCII characters. However, those shouldn't require escaping.
+    testEscape("\uD83D\uDE3B\u2665\u00E4");
+  });
+})();
