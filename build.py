fb1e287fa714af81c75957e470fe941a18b73af1
Francisco Lopez <francisco@adblockplus.org>
2015-01-15 12:23:53 +0100

    Issue 1757 - Integrate ensure_dependencies.py
    --HG--
    extra : rebase_source : 732fc5c03b467c3a4523b47d1bf36bf4eb9252f5

--- build.py
+++ build.py
@@ -3,5 +3,13 @@
 
 import os, sys, subprocess
 
+DEPENDENCY_SCRIPT = os.path.join(os.path.dirname(__file__), "ensure_dependencies.py")
+
+try:
+  subprocess.check_call([sys.executable, DEPENDENCY_SCRIPT])
+except subprocess.CalledProcessError as e:
+  print >>sys.stderr, e
+  print >>sys.stderr, "Failed to ensure dependencies being up-to-date!"
+
 import buildtools.build
 buildtools.build.processArgs('.', sys.argv)
