158874e9bddcada23ebca23ea0a80942171667bd
Sebastian Noack <sebastian@adblockplus.org>
2016-04-18 17:55:44 +0200

    Issue 3952 - Fix whitespaces in build.py for compliance with PEP-8

--- build.py
+++ build.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os, sys, subprocess
+import os
+import sys
+import subprocess
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DEPENDENCY_SCRIPT = os.path.join(BASE_DIR, "ensure_dependencies.py")
 
 try:
-  subprocess.check_call([sys.executable, DEPENDENCY_SCRIPT, BASE_DIR])
+    subprocess.check_call([sys.executable, DEPENDENCY_SCRIPT, BASE_DIR])
 except subprocess.CalledProcessError as e:
-  print >>sys.stderr, e
-  print >>sys.stderr, "Failed to ensure dependencies being up-to-date!"
+    print >>sys.stderr, e
+    print >>sys.stderr, "Failed to ensure dependencies being up-to-date!"
 
 import buildtools.build
 buildtools.build.processArgs(BASE_DIR, sys.argv)
