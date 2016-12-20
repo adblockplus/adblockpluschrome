067ed8ef1c317a8a1ea1222cde1d8a5f3a58e424
Felix Dahlke <felix@adblockplus.org>
2016-12-20 22:03:16 +0100

    Issue 4659 - Incorporate element hiding emulation filters

--- include.preload.js
+++ include.preload.js
@@ -430,13 +430,13 @@ function ElemHide()
   this.style = null;
   this.tracer = null;
 
-  this.propertyFilters = new CSSPropertyFilters(
+  this.elemHideEmulation = new ElemHideEmulation(
     window,
     function(callback)
     {
       ext.backgroundPage.sendMessage({
         type: "filters.get",
-        what: "cssproperties"
+        what: "elemhideemulation"
       }, callback);
     },
     this.addSelectors.bind(this)
@@ -544,11 +544,11 @@ ElemHide.prototype = {
   apply: function()
   {
     var selectors = null;
-    var propertyFiltersLoaded = false;
+    var elemHideEmulationLoaded = false;
 
     var checkLoaded = function()
     {
-      if (!selectors || !propertyFiltersLoaded)
+      if (!selectors || !elemHideEmulationLoaded)
         return;
 
       if (this.tracer)
@@ -560,7 +560,7 @@ ElemHide.prototype = {
       this.style = null;
 
       this.addSelectors(selectors.selectors);
-      this.propertyFilters.apply();
+      this.elemHideEmulation.apply();
 
       if (selectors.trace)
         this.tracer = new ElementHidingTracer(selectors.selectors);
@@ -572,9 +572,9 @@ ElemHide.prototype = {
       checkLoaded();
     });
 
-    this.propertyFilters.load(function()
+    this.elemHideEmulation.load(function()
     {
-      propertyFiltersLoaded = true;
+      elemHideEmulationLoaded = true;
       checkLoaded();
     });
   }
