a173650176969bff3f2b5e107a9401f73579e5b8
Manish Jethani <manish@adblockplus.org>
2019-01-31 05:29:05 +0100

    Issue 7243 - Update adblockpluscore dependency to hg:e26e122e0702

--- qunit/tests/url.js
+++ qunit/tests/url.js
@@ -18,7 +18,7 @@
 "use strict";
 
 {
-  const {extractHostFromFrame, isThirdParty} = require("../../lib/url");
+  const {extractHostFromFrame} = require("../../lib/url");
   const {platform} = require("info");
 
   QUnit.module("URL/host tools");
@@ -58,85 +58,5 @@
       testFrameHostname(["http://user:password@example.com/"], "example.com",
                         "with auth credentials");
     }
-  });
-
-  test("Third-party checks", () =>
-  {
-    function hostnameToURL(hostname)
-    {
-      return new URL("http://" + hostname);
-    }
-
-    function testThirdParty(requestHost, documentHost, expected, message)
-    {
-      equal(
-        isThirdParty(
-          hostnameToURL(requestHost),
-
-          // Chrome's URL object normalizes IP addresses. So some test
-          // will fail if we don't normalize the document host as well.
-          hostnameToURL(documentHost).hostname
-        ),
-        expected,
-        message
-      );
-    }
-
-    testThirdParty("foo", "foo", false, "same domain isn't third-party");
-    testThirdParty("foo", "bar", true, "different domain is third-party");
-    testThirdParty("foo.com", "foo.com", false,
-                   "same domain with TLD (.com) isn't third-party");
-    testThirdParty("foo.com", "bar.com", true,
-                   "same TLD (.com) but different domain is third-party");
-    testThirdParty("foo.com", "www.foo.com", false,
-                   "same domain but differend subdomain isn't third-party");
-    testThirdParty("foo.example.com", "bar.example.com", false,
-                   "same basedomain (example.com) isn't third-party");
-    testThirdParty("foo.uk", "bar.uk", true,
-                   "same TLD (.uk) but different domain is third-party");
-    testThirdParty("foo.co.uk", "bar.co.uk", true,
-                   "same TLD (.co.uk) but different domain is third-party");
-    testThirdParty("foo.example.co.uk", "bar.example.co.uk", false,
-                   "same basedomain (example.co.uk) isn't third-party");
-    testThirdParty("1.2.3.4", "1.2.3.4", false,
-                   "same IPv4 address isn't third-party");
-    testThirdParty("1.1.1.1", "2.1.1.1", true,
-                   "different IPv4 address is third-party");
-    testThirdParty("0x01ff0101", "0x01ff0101", false,
-                   "same IPv4 hexadecimal address isn't third-party");
-    testThirdParty("0x01ff0101", "0x01ff0102", true,
-                   "different IPv4 hexadecimal address is third-party");
-    testThirdParty(
-      "1.0xff.3.4", "1.0xff.3.4", false,
-      "same IPv4 address with hexadecimal octet isn't third-party"
-    );
-    testThirdParty(
-      "1.0xff.1.1", "2.0xff.1.1", true,
-      "different IPv4 address with hexadecimal octet is third-party"
-    );
-    testThirdParty(
-      "0xff.example.com", "example.com", false,
-      "domain starts like a hexadecimal IPv4 address but isn't one"
-    );
-    testThirdParty(
-      "[2001:db8:85a3::8a2e:370:7334]", "[2001:db8:85a3::8a2e:370:7334]", false,
-      "same IPv6 address isn't third-party"
-    );
-    testThirdParty(
-      "[2001:db8:85a3::8a2e:370:7334]", "[5001:db8:85a3::8a2e:370:7334]", true,
-      "different IPv6 address is third-party"
-    );
-    testThirdParty(
-      "[::ffff:192.0.2.128]", "[::ffff:192.0.2.128]", false,
-      "same IPv4-mapped IPv6 address isn't third-party"
-    );
-    testThirdParty(
-      "[::ffff:192.0.2.128]", "[::ffff:192.1.2.128]", true,
-      "different IPv4-mapped IPv6 address is third-party"
-    );
-    testThirdParty("xn--f-1gaa.com", "f\u00f6\u00f6.com", false,
-                   "same IDN isn't third-party");
-    testThirdParty("example.com..", "example.com....", false,
-                   "traling dots are ignored");
   });
 }
