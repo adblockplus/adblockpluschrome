5591d751a40ce1b3184f1ef2d0f28ba73e50ff96
tonimellodic <tonimellodic@gmail.com>
2020-07-03 11:32:30 +0100

    Fixes #208 - Add unit tests for uninstalling URL

--- qunit/tests/uninstall.js
+++ qunit/tests/uninstall.js
@@ -19,12 +19,16 @@
 
 {
   const {analytics} = require("../../adblockpluscore/lib/analytics");
+  const {filterStorage} = require("../../adblockpluscore/lib/filterStorage");
+  const {Prefs} = require("../../lib/prefs");
   const {setUninstallURL} = require("../../lib/uninstall");
 
   const realSetUninstallURL = browser.runtime.setUninstallURL;
   let uninstallURL;
 
-  QUnit.module("Uninstall Link", {
+  let urlParams = () => new URL(uninstallURL).search.substr(1).split("&");
+
+  QUnit.module("Uninstall URL", {
     beforeEach()
     {
       browser.runtime.setUninstallURL = url => uninstallURL = url;
@@ -35,16 +39,96 @@
     }
   });
 
-  QUnit.test("firstVersion parameter in uninstall URL", assert =>
+  QUnit.test("parameters in uninstall URL", assert =>
   {
+    const info = require("info");
+    const expectedParams = [
+      ["an", info.addonName],
+      ["av", info.addonVersion],
+      ["ap", info.application],
+      ["apv", info.applicationVersion],
+      ["p", info.platform],
+      ["fv", analytics.getFirstVersion()],
+      ["pv", info.platformVersion],
+      ["ndc", "0"],
+      ["c", "0"],
+      ["s", "3"]
+    ];
     setUninstallURL();
 
-    let params = new URL(uninstallURL).search.substr(1).split("&");
-    let firstVersion = analytics.getFirstVersion();
+    let params = urlParams();
+    for (let [name, value] of expectedParams)
+    {
+      value = encodeURIComponent(value);
+      assert.ok(
+        params.includes(`${name}=${value}`),
+        `The parameter '${name}' has the expected value '${value}'`
+      );
+    }
+  });
 
+  QUnit.test("uninstall URL length", assert =>
+  {
+    const maxLength = 255;
+    setUninstallURL();
     assert.ok(
-      params.includes("fv=" + encodeURIComponent(firstVersion)),
-      "The firstVersion parameter is included correctly"
+      uninstallURL.length <= maxLength,
+      `uninstall URL is not longer than ${maxLength} characters`
+    );
+  });
+
+  let initialSubscriptions;
+
+  QUnit.module("subscription parameter", {
+    beforeEach()
+    {
+      browser.runtime.setUninstallURL = url => uninstallURL = url;
+      initialSubscriptions = Array.from(filterStorage.subscriptions());
+    },
+    afterEach()
+    {
+      for (let subscription of initialSubscriptions)
+        filterStorage.addSubscription(subscription);
+      browser.runtime.setUninstalLURL = realSetUninstallURL;
+    }
+  });
+
+  QUnit.test("parameter s=0", assert =>
+  {
+    for (let subscription of initialSubscriptions)
+      filterStorage.removeSubscription(subscription);
+    setUninstallURL();
+    assert.ok(
+      urlParams().includes("s=0"),
+      "subscription parameter 's' has the expected value '0'"
+    );
+  });
+
+  QUnit.test("parameter s=1", assert =>
+  {
+    for (let subscription of initialSubscriptions)
+    {
+      if (subscription.type != "ads")
+        filterStorage.removeSubscription(subscription);
+    }
+    setUninstallURL();
+    assert.ok(
+      urlParams().includes("s=1"),
+      "subscription parameter 's' has the expected value '1'" + urlParams()
+    );
+  });
+
+  QUnit.test("parameter s=2", assert =>
+  {
+    for (let subscription of initialSubscriptions)
+    {
+      if (subscription.url != Prefs.subscriptions_exceptionsurl)
+        filterStorage.removeSubscription(subscription);
+    }
+    setUninstallURL();
+    assert.ok(
+      urlParams().includes("s=2"),
+      "subscription parameter 's' has the expected value '2'" + urlParams()
     );
   });
 }
